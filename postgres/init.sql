2414852152a2a41386dcf20bc7aa327dc45ef09b
Rafael93150 <tavaresrafael93@gmail.com>
2023-05-11 20:19:39 +0200

    update init.sql on branch

--- postgres/init.sql
+++ postgres/init.sql
@@ -8,6 +8,13 @@ CREATE TABLE todo (
 INSERT INTO todo (name) VALUES ('Faire les courses');
 INSERT INTO todo (name) VALUES ('Faire le ménage');
 INSERT INTO todo (name) VALUES ('Répondre aux e-mails');
+INSERT INTO todo (name) VALUES ('Préparer la réunion');
+INSERT INTO todo (name) VALUES ('Faire la vaisselle');
+INSERT INTO todo (name) VALUES ('Aller chez le médecin');
+INSERT INTO todo (name) VALUES ('Réserver le vol');
+INSERT INTO todo (name) VALUES ('Réparer la machine à laver');
+INSERT INTO todo (name) VALUES ('Préparer le dîner');
+INSERT INTO todo (name) VALUES ('Sortir les poubelles');
 
 -- CREATE TABLE todo (
 --     id SERIAL PRIMARY KEY,
